040b7ad6effe2f2af0b9c120c728bfea773c8730
Frank Bryce <jonnyjack7@gmail.com>
2017-10-22 04:05:09 +0000

    updating the state of the code before trying to run it from another spoton another computer

--- src/driver.go
+++ src/driver.go
@@ -4,91 +4,28 @@ import (
     "fmt"
     "keith"
     "math/big"
-    "time"
+    //"time"
 )
 
 func main() {
-    /*
-    keiths := []*big.Int{}
-    z := big.NewInt(0)
-    inc := big.NewInt(int64(1))
-    upto := big.NewInt(int64(1 << 25))
-    for i := big.NewInt(10); i.Cmp(upto) <= 0; i.Add(i, inc) {
-        if keith.IsKeith(i) {
-            keiths = append(keiths, big.NewInt(0).Add(z, i))
-        }
+    n := big.NewInt(int64(10000000000000))
+    n.Mul(n, n)
+    n.Mul(n, big.NewInt(10))
+    
+    zero := big.NewInt(0)
+    one := big.NewInt(1)
+    nineteen := big.NewInt(19)
+    for mod := big.NewInt(0).Mod(n, nineteen); mod.Cmp(zero) != 0; mod = big.NewInt(0).Mod(n, nineteen) {
+        n.Add(n, one)
     }
-    fmt.Println(keiths)
-    */
-    fmt.Println(keith.IsKeith(big.NewInt(14)))
-    n := big.NewInt(int64(1 << 62))
-    start := time.Now()
-    isKeith := keith.IsKeith(n)
-    elapsed := time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
-    n.Mul(n, n)
-    start = time.Now()
-    isKeith = keith.IsKeith(n)
-    elapsed = time.Since(start)
-    fmt.Printf("%v digits: %v (in %v)\n", len(n.String()), isKeith, elapsed)
+    
+    c := int64(0)
+    for ;!keith.IsKeith(n); n.Add(n, nineteen) {
+        if c % 100000 == 0 {
+            fmt.Println("%v : %v digits", n, len(n.String()))
+        }
+        c++
+    }
+    
+    fmt.Println("%v : %v digits", n, len(n.String()))
 }
